b64665ee36e9cbd16753927ab270e23338329978
Zentify-HQ <dwiyovihaidar@gmail.com>
2026-07-20 11:29:04 +0700

    Add VisitorCounter middleware

---
+++ app/Http/Middleware/VisitorCounter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\Visitor;
+use Illuminate\Support\Facades\DB;
+
+class VisitorCounter
+{
+    public function handle($request, Closure $next)
+    {
+        $today = date('Y-m-d');
+        
+        $visitor = Visitor::where('date', $today)->first();
+        
+        if ($visitor) {
+            $visitor->increment('count');
+        } else {
+            Visitor::create([
+                'date' => $today,
+                'count' => 1
+            ]);
+        }
+        
+        return $next($request);
+    }
+}
