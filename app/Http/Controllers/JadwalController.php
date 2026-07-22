cbe3b4e024ededfa41a966a9394a7f4226c3c725
Zentify-HQ <dwiyovihaidar@gmail.com>
2026-07-22 08:10:01 +0700

    menganti detail jadwal dokter

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -11,12 +11,10 @@ class JadwalController extends Controller
         $jadwal = JadwalDokter::all();
         return view('jadwal', compact('jadwal'));
     }
+
     public function show($id)
     {
         $dokter = JadwalDokter::findOrFail($id);
-        $hari = $dokter->hari_praktik;
-        $libur = PengumumanLibur::where('dokter_id', $id)->get();
-        
-        return view('jadwal-detail', compact('dokter', 'hari', 'libur'));
+        return view('jadwal-detail', compact('dokter'));
     }
 }
